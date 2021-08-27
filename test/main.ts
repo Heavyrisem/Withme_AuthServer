a3230a2566495e42c047d0ef173d50734d6756cf
heavyrisem <heavyrisem@gmail.com>
2021-08-28 00:41:18 +0900

    Auth socket route

--- test/main.ts
+++ test/main.ts
@@ -1,7 +1,7 @@
 import io from 'socket.io-client';
 
 console.log("test");
-let socket = io("http://localhost:3000", {query: {mobileID: "TestID"}, path: '/auth'});
+let socket = io("https://withme.heavyrisem.xyz/auth", {query: {mobileID: "TestID"}});
 
 socket.on('connect', () => {
     console.log("conn");
